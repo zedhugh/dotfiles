206d3e207dd37ef8f5ae951ef8ace09db9cbce0f
Zedhugh Chen <zedhugh@gmail.com>
2021-08-10 00:44:00 +0800

    添加git忽略文件变化相关命令别名

--- awesome/pravitekeys.lua
+++ awesome/pravitekeys.lua
@@ -35,7 +35,8 @@ pravitekeys = awful.util.table.join(
 
    awful.key({ modkey }, "e",
       function ()
-         run_or_raise("emacsclient -a '' -c -n", {class = "Emacs"})
+         -- run_or_raise("emacsclient -a '' -c -n", {class = "Emacs"})
+         run_or_raise("emacs", {class = "Emacs"})
    end),
 
    awful.key({ modkey, "Shift" }, "e",
