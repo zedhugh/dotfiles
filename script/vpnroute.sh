6f75857fd76c37255d4f3e564a06ee79fe8da4ef
Zedhugh Chen <zedhugh@gmail.com>
2022-06-26 20:20:42 +0800

    添加 vpn 路由表更改脚本

---
+++ script/vpnroute.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [[ "$1" != "set" ]] && [[ "$1" != "unset" ]]; then
+    echo "Usage: $0 <set|unset>"
+    exit 1;
+fi
+
+if [[ "$UID" != "0" ]]; then
+    echo "$0: you must run this as root"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+DEV=vpn_$(./vpncmd localhost /client /cmd accountlist | grep -e '网络适配器\|Adapter' | cut -d '|' -f 2)
+
+echo $DEV
+
+if [[ "$1" == "set" ]]; then
+    dhcpcd -k $DEV
+    dhcpcd $DEV
+    GW=$(ip route show | grep "default.*$DEV" | awk '{print $3}')
+    sleep 1
+    ip route del default dev $DEV
+    ip route add 192.168.0.0/24 via $GW
+fi
+
+if [[ "$1" == "unset" ]]; then
+    dhcpcd -k $DEV
+fi
