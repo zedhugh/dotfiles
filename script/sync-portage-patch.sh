2ab6f9fcc2e95ead0c809afe8c79aabe6913d6a3
Zedhugh Chen <zedhugh@gmail.com>
2022-07-04 02:23:47 +0800

    添加 slock 补丁

--- script/sync-portage-patch.sh
+++ script/sync-portage-patch.sh
@@ -8,7 +8,8 @@ fi
 
 SCRIPT_DIR=$(dirname $0)
 
-SOURCE_DIR="${SCRIPT_DIR}/../gentoo_portage/patches/x11-terms/st/"
-DEST_DIR="/etc/portage/patches/x11-terms/st"
+# 此处目录后面的 / 不能少
+SOURCE_DIR="${SCRIPT_DIR}/../gentoo_portage/patches/"
+DEST_DIR="/etc/portage/patches/"
 
 rsync -r --delete --mkpath $SOURCE_DIR $DEST_DIR
